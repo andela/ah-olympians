8f98fd461e8cf6a99563be279b701c6c68053f56
Kelvin Onkundi <kelvin.onkundi@andela.com>
2019-02-25 17:32:38 +0300

    chore(upgrade-django): upgrades django from django 1.10 to Django 2.1
     - upgrade djangorestframework
     - upgrade Django from 1.10 to Django 2.1
     - Made changes in files to conform with Django version
    
    [Fixes #164047048]

--- authors/settings.py
+++ authors/settings.py
@@ -137,7 +137,11 @@ REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'authors.apps.core.exceptions.core_exception_handler',
     'NON_FIELD_ERRORS_KEY': 'error',
 
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'authors.apps.authentication.backends.JWTAuthentication',
+    # ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'authors.apps.authentication.backends.JWTAuthentication',
-    ),
+       'rest_framework.authentication.BasicAuthentication',
+       'rest_framework.authentication.SessionAuthentication',
+   )
 }
