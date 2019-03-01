ff0852b42fb222b559c64556f84c13bf6b8d8fa1
Kelvin Onkundi <kelvin.onkundi@andela.com>
2019-03-01 11:44:01 +0300

    feature(social-login) Enable login using social sites
    - User can login with Facebook
    - Users can login in with Google
    
    [Delivers #164047062]

--- authors/settings.py
+++ authors/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'corsheaders',
     'django_extensions',
     'rest_framework',
+    'oauth2_provider',
+    'social_django',
+    'rest_framework_social_oauth2',
 
     'authors.apps.authentication',
     'authors.apps.core',
@@ -70,6 +73,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -146,5 +152,48 @@ REST_FRAMEWORK = {
     #     'authors.apps.authentication.backends.JWTAuthentication',
     # ),
 }
+AUTHENTICATION_BACKENDS = [
+    'rest_framework_social_oauth2.backends.DjangoOAuth2',
+   'django.contrib.auth.backends.ModelBackend',
+]
+
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '65389949271-ppl7dcmr3qbt569a3387vgfgugj7f0a5.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'GOgnvguu7FDUACvs__XiAYQ_'
+#Scope
+SOCIAL_AUTH_GOOGLE_OAUTH_SCOPE = ['email', 'username']
+
+SOCIAL_AUTH_FACEBOOK_KEY = '388837494997867'
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+SOCIAL_AUTH_FACEBOOK_SECRET = '9580a3e99e1f94e122450bc3500441e7'
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
+    'fields': 'id, name, email'
+}
+FACEBOOK_EXTENDED_PERMISSIONS = ['email']
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
+
+AUTHENTICATION_BACKENDS = (
+    # Google OAuth2
+    'social_core.backends.google.GoogleOAuth2',
+    # Facebook OAuth2
+    'social_core.backends.facebook.FacebookAppOAuth2',
+    'social_core.backends.facebook.FacebookOAuth2',
+    # Django
+    'django.contrib.auth.backends.ModelBackend',
+)
+
 # activate django_heroku
 django_heroku.settings(locals())
