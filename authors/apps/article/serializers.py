1cf0a1710a4d3f0848244b8cb20e1ceffd987885
Kelvin Onkundi <kelvin.onkundi@andela.com>
2019-03-19 14:20:45 +0300

    add space after an import

--- authors/apps/article/serializers.py
+++ authors/apps/article/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 from rest_framework import response
 
 from authors.apps.authentication.serializers import UserSerializer
-from .models import Article, ArticleLikes, Rate,ArticleComment
+from .models import Article, ArticleLikes, Rate, ArticleComment
 from ..profiles.serializers import ProfileSerializer
 
 
